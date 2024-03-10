b5e4e0b50c8aa851dc441fcf633612be80a403af
VanHuy79 <ngo.huy.van.2001@gmail.com>
2024-03-10 15:10:42 +0700

    chia component home page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
+  <Header />
   <router-view />
+  <Footer />
 </template>
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
+export default {
+  components: { Footer, Header }
+};
+</script>
 <style lang="scss"></style>
